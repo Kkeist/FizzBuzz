eb65ee299400316711fc46076e70f28f56bd503d
Kkeist <93057353+Kkeist@users.noreply.github.com>
2025-09-08 10:11:17 -0400

    refactored to extract helper method

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -7,7 +7,12 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        int i=1;
+        doFizzBuzz(i);
+    }
+
+    private static void doFizzBuzz(int i) {
+        while (i <100) {
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
@@ -31,6 +36,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
